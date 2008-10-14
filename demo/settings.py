ca252a66fb20fab0b93ff5a8886b7332a470176e
tualatrix <tualatrix@c6481180-99c3-11dd-93a9-677c3c677ea2>
2008-10-14 09:40:30 +0000

    Rename pulog to demo
    git-svn-id: https://pulog.googlecode.com/svn/trunk@7 c6481180-99c3-11dd-93a9-677c3c677ea2

--- demo/settings.py
+++ demo/settings.py
@@ -64,7 +64,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
 )
 
-ROOT_URLCONF = 'pulog.urls'
+ROOT_URLCONF = 'demo.urls'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
