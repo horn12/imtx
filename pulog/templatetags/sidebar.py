4f369551f097362bb1d81802ac91f0b67f701775
tualatrix <tualatrix@c6481180-99c3-11dd-93a9-677c3c677ea2>
2008-12-31 14:55:18 +0000

    Fix the archives tag
    git-svn-id: https://pulog.googlecode.com/svn/trunk@83 c6481180-99c3-11dd-93a9-677c3c677ea2

--- pulog/templatetags/sidebar.py
+++ pulog/templatetags/sidebar.py
@@ -28,34 +28,6 @@ def get_categories(context):
     return {'categories': Category.objects.all(),
         'posts': Post.objects.get_post()}
 
-class MonthArchive:
-    title = ""
-    link = ""
-    year = 0
-    month = 0
-
-    def __init__(self, month):
-        date = month.split("-")
-        self.year = int(date[0])
-        self.month = int(date[1])
-        
-        self.title = datetime.date(self.year, self.month, 1).strftime(_('%B %Y'))
-        self.link = '/archives/%s/%s/' % (date[0], date[1])
-
-    def get_post_count(self):
-        return Post.objects.get_post_by_date(self.year, self.month).count()
-
-@register.inclusion_tag('sidebar/archive_list.html', takes_context = True)
+@register.inclusion_tag('sidebar/archive_list.html')
 def get_archive(context):
-    months = []
-    archive_months = []
-
-    for post in Post.objects.all():
-        month = post.date.date().strftime('%Y-%m')
-        if month not in months:
-            months.append(month)
-
-    for month in months:
-        archive_months.insert(0, MonthArchive(month))
-
-    return {'months': archive_months}
+    return {'months': Post.objects.dates('date', 'month')}
