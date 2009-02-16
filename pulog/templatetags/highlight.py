d99cfcc13e63641564216e0e2b5b755a2005dfca
tualatrix <tualatrix@c6481180-99c3-11dd-93a9-677c3c677ea2>
2009-02-16 14:12:06 +0000

    Finish the Code Highlight
    git-svn-id: https://pulog.googlecode.com/svn/trunk@169 c6481180-99c3-11dd-93a9-677c3c677ea2

--- pulog/templatetags/highlight.py
+++ pulog/templatetags/highlight.py
@@ -6,28 +6,24 @@ from pygments.formatters import HtmlFormatter
 
 register = template.Library()
 
-#pre = re.compile(r'<pre( lang=\w*).*>.*</pre>')
-p_pre = re.compile(r'<pre (lang=\w+).*?>(?P<code>[\w\W]+?)</pre>')
-#pre = re.compile(r'<pre( lang=\w*?).*?>.*?</pre>')
+p_pre = re.compile(r'<pre (?=lang=[\'"]?\w+[\'"]?).*?>(?P<code>[\w\W]+?)</pre>')
 p_lang = re.compile(r'lang=[\'"]?(?P<lang>\w+)[\'"]?')
 
 def code2html(code, lang):
-    lexer = get_lexer_by_name(lang, encoding='utf-8', stripall=True)
-    formatter = HtmlFormatter(encoding='utf-8')
-    #            full = True,
-    #            linenos=False,
-    #            noclasses="True")
-    result = highlight(code, lexer, formatter)
-    return result
+    lexer = get_lexer_by_name(lang, encoding = 'utf-8', stripall = True)
+    formatter = HtmlFormatter(encoding = 'utf-8')
+    return highlight(code, lexer, formatter)
 
 @register.filter
 def do_highlight(value):
-    m = p_pre.search(value)
-    print 'is m here', m
-    if m:
-        pre_block = m.group()
-        lm = p_lang.search(pre_block)
-        if lm:
-            lang = lm.group('lang')
-            return p_pre.sub(code2html(m.group('code'), lang), value)
-    return value
+    s_list = p_pre.split(value)
+    f_list = p_pre.findall(value)
+    if f_list:
+        for code_block in p_pre.finditer(value):
+            lang = p_lang.search(code_block.group()).group('lang')
+            code = code_block.group('code')
+            index = s_list.index(code)
+
+            s_list[index] = code2html(code, lang)
+
+    return ''.join(s_list)
