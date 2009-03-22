382098ab9d7765058a96dcdcee90dafb1847e3b6
tualatrix <tualatrix@c6481180-99c3-11dd-93a9-677c3c677ea2>
2009-03-22 11:53:36 +0000

    Clean the old folders about pulog
    git-svn-id: https://pulog.googlecode.com/svn/trunk@196 c6481180-99c3-11dd-93a9-677c3c677ea2

--- demo-site/static/js/tiny_mce/plugins/pulog_upload/langs/en_dlg.js
+++
@@ -1,3 +0,0 @@
-tinyMCE.addI18n('en.pulog_upload_dlg',{
-	title : 'pulog upload window'
-});
--- pulog/templatetags/page.py
+++
@@ -1,8 +0,0 @@
-from django import template
-from pulog.models import Post
-
-register = template.Library()
-
-@register.tag
-def list_pages(context):
-	return {'pages': Post.objects.get_page()}
