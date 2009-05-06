407d37e8d0d9a46cafa9baee72226c28bb07c04d
huacnlee <huacnlee@c6481180-99c3-11dd-93a9-677c3c677ea2>
2009-05-06 15:44:47 +0000

    1. caches 加入tag的概念,可以为每个 cache 另起一个tag名称,这种应有一般用在列表分页的时候,如 page/1,page/2 ... page/100 这么多个 cache key,如果想一次删除,没有tag或namespace 之类的东西,哪就不好管理,这次加入的功能就是为这些 cache key 加一个tag 如 page ,然后就可以用 caches.delete_by_tags(['page']) 来删除 page/1,page/2 ... page/100 的缓存.
    git-svn-id: https://pulog.googlecode.com/svn/trunk@233 c6481180-99c3-11dd-93a9-677c3c677ea2

--- imtx/utils/caches.py
+++ imtx/utils/caches.py
@@ -3,19 +3,59 @@
 #
 from django.core.cache import cache
 from imtx.settings import *
+import logs
+
+# stroe the cache keys for group delete
+tag_list = []
 
 def _format_key(key):
-	"""add prefix to key"""
-	return '%s/%s' % (CACHE_PREFIX,key)
+    """add prefix to key"""
+    return '%s/%s' % (CACHE_PREFIX,key)
 
-def set(key,value):
-	return cache.set(_format_key(key),value)
-	
+def _set(key,value, timeout=None):
+    logs.debug('cache set %s' % key)
+    return cache.set(_format_key(key),value)
+    
 def get(key):
-	return cache.get(_format_key(key))
-	
+    obj = cache.get(_format_key(key))
+    if obj:
+        logs.debug('cache hit %s' % key)
+    
 def delete(key):
-	cache.delete(_format_key(key))
-	
+    logs.debug('cache delete %s' % key)
+    cache.delete(_format_key(key))
+    
 def get_many(keys = []):
-	cache.get_many(keys.e)
+    cache.get_many(keys.e)
+    
+def set(key, value, tags=[], timeout=None):
+    """
+	author: http://stepsandnumbers.com/archive/2009/04/11/setting-and-delete-cache-in-django-with-tags/
+    set namespace with cache group
+    eg:
+        set_with_tags('home/list/p/1',list,['home/list'])
+        set_with_tags('home/list/p/2',list,['home/list'])
+    """
+    for tag in tags:
+        tag_list = get(tag)
+        if tag_list:
+            tag_list.append(key)
+        else:
+            tag_list = [key]
+        _set(tag, tag_list, timeout)
+    _set(key, value, timeout)
+
+def delete_by_tags(tags=[]):
+    """
+	author: http://stepsandnumbers.com/archive/2009/04/11/setting-and-delete-cache-in-django-with-tags/
+	delete caches by namespace/tag
+	eg:
+		delete_by_tags(['home/list'])
+		# this will delete home/list/p/1,home/list/p/2 ...
+	"""
+    for tag in tags:
+        tag_list = get(tag)
+        if tag_list:
+           for key in tag_list:
+               delete(key)
+        delete(tag)
