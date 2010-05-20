9799b02b6c7fe23f74a7c312cff742b7a32318f8
TualatriX <tualatrix@gmail.com>
2010-05-20 19:54:16 +0800

    Use env to control user and hosts

--- imtx/fabfile.py
+++ imtx/fabfile.py
@@ -1,6 +1,10 @@
 from __future__ import with_statement
+import os
 from fabric.api import *
 from fabric.contrib.console import confirm
+
+env.user = os.getenv('IMTX_USER')
+env.hosts = [os.getenv('IMTX_HOST')]
 
 def deploy():
     with cd('www/imtx.cn/imtx.cn/'):
