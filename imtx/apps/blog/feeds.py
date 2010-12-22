95266cb902354e9e6c35fd98d7fac136138abdf2
Tualatrix Chou <tualatrix@gmail.com>
2010-12-22 22:15:26 +0800

    Limit the comment items to 50

--- imtx/apps/blog/feeds.py
+++ imtx/apps/blog/feeds.py
@@ -71,7 +71,7 @@ class LatestCommentFeed(Feed):
     description_template = 'feed/latest_comment_description.html'
 
     def items(self):
-        return Comment.objects.in_public()
+        return Comment.objects.in_public()[:50]
         
     def item_pubdate(self, item):
         return item.date
